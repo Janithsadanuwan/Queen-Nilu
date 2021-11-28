47a341aebdfacd583b35305e075731ac412ef178
Janith sadanuwan <95123183+Janithsadanuwan@users.noreply.github.com>
2021-11-28 18:20:09 +0530

    💙𝗤𝘂𝗲𝗲𝗻 𝗡𝗶𝗹𝘂💙

---
+++ QueenNilu_modules/index.js
@@ -0,0 +1,15 @@
+/*
+
+Copyright (C) 2021 💙𝗤𝘂𝗲𝗲𝗻 𝗡𝗶𝗹𝘂💙.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
